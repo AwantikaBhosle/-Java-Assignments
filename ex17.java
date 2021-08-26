d9d19d62503d80ea99f09cc099ae8818ce461c6c
AwantikaBhosle <89515531+AwantikaBhosle@users.noreply.github.com>
2021-08-26 11:25:53 +0530

    Add files via upload

---
+++ ex17.java
@@ -0,0 +1,18 @@
+//Write a program in JAVA to find the Area and Perimeter of a Rectangle.   
+import java.util.Scanner;
+public class Area_Perimeter 
+{
+    public static void main(String[] args) 
+    {
+        int l, b, perimeter, area;
+        Scanner s =  Scanner(System.in);
+        System.out.print("Input length of rectangle:");
+        l = s.nextInt();
+        System.out.print("Input breadth of rectangle:");
+        b = s.nextInt();
+        perimeter = 2 * (l + b);
+        System.out.println(" The Perimeter of rectangle:"+perimeter);
+        area = l * b;
+        System.out.println("The Area of rectangle:"+area);
+    }
+} 
