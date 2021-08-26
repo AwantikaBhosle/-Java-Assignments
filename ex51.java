2d129cbf1274865b403b2227eac1d1b29a6974c6
AwantikaBhosle <89515531+AwantikaBhosle@users.noreply.github.com>
2021-08-26 13:04:00 +0530

    Add files via upload

---
+++ ex51.java
@@ -0,0 +1,15 @@
+//Write a program in JAVA that converts kilometers per hour to miles per hour.   
+
+import java.util.Scanner;
+class DistanceConversion {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		double kmph, miph;
+		System.out.println("Convert kilometers per hour to miles per hour:");
+		System.out.println("-------------------------------------");
+		System.out.print("Input the distance in kilometer:");
+		kmph = sc.nextFloat();
+		miph = (kmph * 0.6213712);
+		System.out.println("The " + kmph + " Km./hr. means " + miph + " Miles/hr.");
+	}
+}
---
+++ ex52.java
@@ -0,0 +1,16 @@
+//Write a program in JAVA to enter two angles of a triangle and find the third angle.   
+import java.util.Scanner;
+class AnglesOfTriangle {
+	public static void main(String args[]) {
+		float ang1, ang2, ang3;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Find the third angle of a triangle:");
+		System.out.println("-------------------------------------");
+		System.out.print("Input the 1st angle of the triangle:");
+		ang1 = sc.nextFloat();
+		System.out.println("Input the 2nd angle of the triangle:");
+		ang2 = sc.nextFloat();
+		ang3 = 180 - (ang1 + ang2);
+		System.out.println("The 3rd of the triangle is:" + ang3);
+	}
+}
